483149832a3918cdf0eec9f366fb9c60e6b953d5
stevenjmarsh <sjm.atl@gmail.com>
2016-11-01 15:24:54 -0400

    adding filter (checkbox) for incomplete tasks

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -14,6 +14,7 @@ class AppContainer extends TrackerContainerComponent {
       subscription: {
         tasks: Meteor.subscribe("tasks"),
       },
+      hideCompleted: false,
     };
   }
 
@@ -37,6 +38,12 @@ class AppContainer extends TrackerContainerComponent {
     Meteor.call("tasks.insert", newTask);
   }
 
+  handleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   render() {
     return (
       <App
@@ -44,6 +51,8 @@ class AppContainer extends TrackerContainerComponent {
         tasks={Tasks.find({}, { sort: { createdAt: -1 } }).fetch()}
         onUpdateTask={event => this.handleUpdateTask(event)}
         onSubmitTask={event => this.handleSubmitTask(event)}
+        hideCompleted={this.state.hideCompleted}
+        onHideCompleted={() => this.handleHideCompleted()}
       />
     );
   }
