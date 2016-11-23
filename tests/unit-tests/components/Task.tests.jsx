0d8734c6f016bac7ca4648b6f5e5dd1f444de930
stevenjmarsh <sjm.atl@gmail.com>
2016-11-23 15:13:28 -0500

    WIP adding more simple / useless tests for practice with enzyme and testdouble

--- tests/unit-tests/components/Task.tests.jsx
+++ tests/unit-tests/components/Task.tests.jsx
@@ -5,34 +5,46 @@ import React from "react";
 import { shallow } from "enzyme";
 import chai, { expect } from "chai";
 import chaiEnzyme from "chai-enzyme";
+import td from "testdouble";
 import Task from "../../../imports/ui/components/Task";
 
 chai.use(chaiEnzyme());
 
 describe("<Task />", function () {
+  // NOT VERY USEFUL TESTS
+  //  -just practice with enzyme and testdouble
+  const task = {
+    username: "testGuy",
+    text: "this test task",
+    private: false,
+    checked: false,
+  };
+  const stubFunc = function () { };
+  const handleToggleCheckedStub = td.function("handleToggleCheckedStub");
+
+  const renderedTask = shallow(
+    <Task
+      task={task}
+      onToggleCheckedTask={() => handleToggleCheckedStub()}
+      onDeleteTask={() => stubFunc()}
+      showPrivateButton={false}
+      onTogglePrivate={() => stubFunc()}
+    />
+  );
+
+  afterEach(function () {
+    td.reset();
+  });
+
   it("should render without private button", function () {
-    const task = {
-      username: "testGuy",
-      text: "this test task",
-      private: false,
-      checked: false,
-    };
-
-    const stubFunc = function () { };
-
-    const renderedTask = shallow(
-      <Task
-        task={task}
-        onToggleCheckedTask={() => stubFunc()}
-        onDeleteTask={() => stubFunc()}
-        showPrivateButton={false}
-        onTogglePrivate={() => stubFunc()}
-      />
-    );
-
     expect(renderedTask).to.not.have.className("checked");
     expect(renderedTask).to.not.have.className("private");
     expect(renderedTask).to.have.descendants("button.delete");
     expect(renderedTask).to.not.have.descendants("button.toggle-private");
+  });
+
+  it("should update status when checked", function () {
+    renderedTask.find("input[type='checkbox']").simulate("click");
+    td.verify(handleToggleCheckedStub());
   });
 });
--- tests/unit-tests/components/TaskContainer.tests.jsx
+++ tests/unit-tests/components/TaskContainer.tests.jsx
@@ -10,13 +10,21 @@ import td from "testdouble";
 chai.use(chaiEnzyme());
 
 describe("<TaskContainer />", function () {
+  // NOT VERY USEFUL TESTS
+  //  -just practice with enzyme and testdouble
   let TaskContainer;
+
+  const task = {
+    _id: "test_task_id",
+    username: "testGuy",
+    text: "this test task",
+    private: false,
+    checked: false,
+  };
 
   beforeEach(function () {
     const methods = td.object(["setCompleted", "removeTask", "setPrivate"]);
-
-    td.replace("../../../imports/api/tasks/methods", { methods });
-
+    td.replace("../../../imports/api/tasks/methods", methods);
     TaskContainer = require("../../../imports/ui/containers/TaskContainer").default;
   });
 
@@ -25,20 +33,12 @@ describe("<TaskContainer />", function () {
   });
 
   it("should render the <Task /> component", function () {
-    const task = {
-      _id: "test_task_id",
-      username: "testGuy",
-      text: "this test task",
-      private: false,
-      checked: false,
-    };
-
     const wrapper = shallow(<TaskContainer task={task} showPrivateButton={false} />);
-
     expect(wrapper.find("Task")).to.have.prop("task").deep.equal(task);
   });
 
-  it("should update status when checked", function () {
-    // follow guide testing example
+  it("should call setCompleted method when clicked", function () {
+    TaskContainer.handleToggleChecked;
+    // TODO td.verify(setCompleted);
   });
 });
