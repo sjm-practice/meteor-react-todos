f75e6ca9cf12ad6d748e2a658f11de99212228d0
stevenjmarsh <sjm.atl@gmail.com>
2016-11-29 15:49:28 -0500

    refactoring test, better organize before and after each code

--- tests/unit-tests/components/Task.tests.jsx
+++ tests/unit-tests/components/Task.tests.jsx
@@ -13,38 +13,45 @@ chai.use(chaiEnzyme());
 describe("<Task />", function () {
   // NOT VERY USEFUL TESTS
   //  -just practice with enzyme and testdouble
-  const task = {
-    username: "testGuy",
-    text: "this test task",
-    private: false,
-    checked: false,
-  };
-  const stubFunc = function () { };
-  const handleToggleCheckedStub = td.function("handleToggleCheckedStub");
+  let task;
+  let stubFunc;
+  let handleToggleCheckedStub;
+  let wrapper;
 
-  const renderedTask = shallow(
-    <Task
-      task={task}
-      onToggleCheckedTask={() => handleToggleCheckedStub()}
-      onDeleteTask={() => stubFunc()}
-      showPrivateButton={false}
-      onTogglePrivate={() => stubFunc()}
-    />
-  );
+  beforeEach(function () {
+    task = {
+      username: "testGuy",
+      text: "this test task",
+      private: false,
+      checked: false,
+    };
+    stubFunc = function () { };
+    handleToggleCheckedStub = td.function("handleToggleCheckedStub");
+
+    wrapper = shallow(
+      <Task
+        task={task}
+        onToggleCheckedTask={() => handleToggleCheckedStub()}
+        onDeleteTask={() => stubFunc()}
+        showPrivateButton={false}
+        onTogglePrivate={() => stubFunc()}
+      />
+    );
+  });
 
   afterEach(function () {
     td.reset();
   });
 
   it("should render without private button", function () {
-    expect(renderedTask).to.not.have.className("checked");
-    expect(renderedTask).to.not.have.className("private");
-    expect(renderedTask).to.have.descendants("button.delete");
-    expect(renderedTask).to.not.have.descendants("button.toggle-private");
+    expect(wrapper).to.not.have.className("checked");
+    expect(wrapper).to.not.have.className("private");
+    expect(wrapper).to.have.descendants("button.delete");
+    expect(wrapper).to.not.have.descendants("button.toggle-private");
   });
 
   it("should update status when checked", function () {
-    renderedTask.find("input[type='checkbox']").simulate("click");
+    wrapper.find("input[type='checkbox']").simulate("click");
     td.verify(handleToggleCheckedStub());
   });
 });
