9114482ae54eb712cb32e46c421306d3aa72cb7b
stevenjmarsh <sjm.atl@gmail.com>
2017-01-19 10:36:09 -0500

    adding more common Meteor package methods to meteor jest stub

--- tests/jest-meteor-stubs/meteor.js
+++ tests/jest-meteor-stubs/meteor.js
@@ -2,6 +2,11 @@
 
 module.exports = {
   Meteor: {
+    Error: jest.fn(),
+    methods: jest.fn(),
+    call: jest.fn(),
+    apply: jest.fn(),
+    publish: jest.fn(),
     subscribe: jest.fn(),
   },
 };
